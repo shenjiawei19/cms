47b6131bb9f1b16c5e8eef59b8a3883bbd8a9b35
shenjiawei <shenjiawei19@sina.com>
2017-04-26 15:10:59 +0800

    cnd质量监控完工

--- cushion/views.py
+++ cushion/views.py
@@ -34,6 +34,16 @@ A21 = ['IDEATV_50E21X','IDEATV_42E31X','IDEATV_42E31Y','IDEATV_32E31Y','IDEATV_3
        'IDEATV_55E31Y','IDEATV_32A21X','IDEATV_32A21Y','IDEATV_42A21X','IDEATV_A21','IDEATV_39E31Y',
        'IDEATV_50E31Y','IDEATV_50E31X','A21','IDEATV_50A21Y','IDEATV_39A21Y','IDEATV_42A21Y','IDEATV_55A21Y',
        'IDEATV_55A21X','IDEATV_55A3Y','IDEATV_42A3Y']
+sky_shape = ['sky_a11a','sky_lcd_s3a01','sky_lcd_uf30a','sky_lcd_xxbel6a_b','sky_lcd_xxbel7a_b','sky_lcd_xxbel8a',
+             'sky_lcd_xxcae5a','sky_lcd_xxcae5a_b','sky_lx460a','sky_lx560a','sky_lx565ab','sky_v3a']
+shy_changhong = ['sky_mt5520_ll','sky_mt5520_ll_15g','sky_mt5520_ll_15g_rtk','sky_mt5520_ll_srs','sky_full_changhong']
+sky_baofeng = ['sky_baofeng_tv']
+sky_tencent = ['sky_full_mstaredison','sky_rk3288']
+sky_kukai = ['sky_kukai']
+sky_kangjia = ['sky_rtd2995d_2d','sky_rtd2995d_6200','sky_rtd2995d_6610','sky_rtd2995d_kktv','sky_rtd2995d_6680',
+              'sky_rtd2995d_reduce','sky_rtd2995d_slim','sky_rtd299x_tv035','sky_rtd2995d_tencenttv']
+sky_lenovo = ['sky_yt-x703f','sky_yt-x703l','sky_yt3_10_prc_lte_ref','sky_yt3_10_prc_wifi_ref']
+
 
 def quality(request):
     cls1 = request.GET.get('class', "")
@@ -49,10 +59,15 @@ def quality(request):
         cls1 == u'检索命中次数' and 10 or cls1 == u'点击视频购买次数' and 11 or cls1 == u'购买成功次数' and 12 or
         cls1 == u'异常次数' and 13]
     support = u'所有' if sup == '' else sup
-    typ = ['K', 'K', 'K82', 'S1', 'S2', 'A21'][
-        typ1 == u'K系列' and 1 or typ1 == u'K82系列' and 2 or typ1 == u'S1系列' and 3 or typ1 == u'S2系列' and 4 or typ1 == u'A21系列' and 5]
+    typ = ['K', 'K', 'K82', 'S1', 'S2', 'A21','shape','changhong','baofeng','tencent','kukai','kangjia','lentv'][
+        typ1 == u'K系列' and 1 or typ1 == u'K82系列' and 2 or typ1 == u'S1系列' and 3 or typ1 == u'S2系列' and 4 or
+        typ1 == u'A21系列' and 5 or typ1 == u'sky夏普' and 6 or typ1 == u'sky长虹' and 7 or typ1 == u'sky暴风' and 8 or
+        typ1 == u'sky腾讯' and 9 or typ1 == u'sky酷开' and 10 or typ1 == u'sky康佳' and 11 or typ1 == u'sky联想' and 12]
     tenDayAgo = (datetime.datetime.now() - datetime.timedelta(days=15)).strftime("%Y-%m-%d")
-    select = [K,K,K82,S1,S2,A21][typ=='K' and 1 or typ== 'K82' and 2 or typ=='S1' and 3 or typ== 'S2'and  4 or typ== 'A21' and 5]
+    select = [K,K,K82,S1,S2,A21,sky_shape,shy_changhong,sky_baofeng,sky_tencent,sky_kukai,sky_lenovo
+              ][typ=='K' and 1 or typ== 'K82' and 2 or typ=='S1' and 3 or typ== 'S2'and  4 or typ== 'A21' and 5 or
+                typ== 'shape' and 6 or typ== 'changhong'and 7 or typ== 'baofeng'and 8 or typ== 'tencent'and 9 or
+                typ== 'kukai'and 10 or typ== 'kangjia'and 11 or  typ== 'lentv'and 12 ]
     if start != '' and over != '' and support != '':
         dd = {'ip': support, 'date__lte': over, 'date__gte': start, 'type__in': select}
     elif support == u'所有' and (cls not in ['uv', 'search', 'arrive', 'expense', 'purchase', 'error']):
